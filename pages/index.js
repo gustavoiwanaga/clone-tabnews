cdf91e91d72e84d4394739b835f74d9fb731f190
Gus Iwanaga <42856613+gustavoiwanaga@users.noreply.github.com>
2026-01-05 18:53:52 +0000

    making minor change

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 function Home(){
-    return <h1>You can rest but you can't stop, aight?</h1>
+    return <h1>You can rest but you can't stop, aight? 
+        Let's see what happens now</h1>
 }
 
 export default Home;
